12e69e9fb929cc30cc9d5ab678eec2371f7f206a
Mario Morales <92040099+mariomo16@users.noreply.github.com>
2022-10-24 13:48:06 +0200

    Add files via upload

---
+++ adivinar_numero.java
@@ -0,0 +1,45 @@
+/*
+ * adivinar_numero.java
+ * 
+ * Copyright 2022 Mario <Mario@DESKTOP-MARIO>
+ * 
+ * 24/10/2022
+ * 
+ */
+
+import java.util.Scanner;
+
+public class adivinar_numero {
+	
+	public static void main (String[] args) {
+		
+		Scanner sc = new Scanner(System.in);	
+		int numero_elegido=37 , numero_leido=0, intentos=1;
+		
+		System.out.println("Escribe un numero entre 0 y 50");
+		numero_leido = sc.nextInt();
+		
+		while (numero_leido<0 || numero_leido>50) {
+			System.out.println("Escribe un numero dentro del rango (0 y 50)");
+			numero_leido = sc.nextInt();
+		}
+		
+		while (numero_elegido<numero_leido) {
+			System.out.println("No acertaste, el numero elegido es menor");
+			intentos++;
+			System.out.println("Escribelo otra vez");
+			numero_leido = sc.nextInt();
+		}
+		
+		while (numero_elegido>numero_leido) {
+			System.out.println("No acertaste, el numero elegido es mayor");
+			intentos++;
+			System.out.println("Escribelo otra vez");
+			numero_leido = sc.nextInt();
+		}
+		
+		System.out.println("El numero correcto es 37!, has tenido " + intentos + " intentos");
+		
+	}
+}
+
