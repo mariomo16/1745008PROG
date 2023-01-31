2eb488f3a9680582608b82c3276ada358434f3b2
Mario Morales <92040099+mariomo16@users.noreply.github.com>
2023-01-31 13:28:01 +0000

    numeros

---
+++ UT03 (Entornos de Desarrollo)/Estadistica/src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Estadistica miNumero = new Estadistica();
+
+        miNumero.getCapicua();
+
+        
+    }
+}
