14094614a38a376962024c5d8b22a7f28583173b
Mario Morales <92040099+mariomo16@users.noreply.github.com>
2023-02-17 12:27:29 +0000

    Actualizado Ejercicio2 y agregado Ejercicio6 (PRO)

--- UT03/Boletín I - Strings/Ejercicio2.java
+++ UT03/Boletín I - Strings/Ejercicio2.java
@@ -4,7 +4,9 @@ public class Ejercicio2 {
 
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
+
 	    System.out.print("Introduce una cadena de texto: ");
+
 	    String cadena = sc.nextLine();
 
 	    String[] palabras = cadena.split(" ");
---
+++ UT03/Boletín I - Strings/Ejercicio6.java
@@ -0,0 +1,32 @@
+// Se importa la clase Scanner para poder leer la entrada del usuario
+import java.util.Scanner;
+
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+        // Se crea un objeto Scanner llamado sc
+        Scanner sc = new Scanner(System.in);
+
+        // Se muestra un mensaje pidiéndole al usuario que introduzca una frase
+        System.out.print("Introduce una frase: ");
+
+        // Se lee la frase introducida por el usuario con el método nextLine() y se asigna a la variable frase
+        // Se convierte la frase a minúsculas con el método toLowerCase() 
+        // y se eliminan los espacios con el método replaceAll("\\s", "").
+        String frase = sc.nextLine().toLowerCase().replaceAll("\\s", "");
+
+        // Se crea una nueva cadena fraseInvertida que es la inversa de la cadena frase 
+        // utilizando la clase StringBuilder y su método reverse().
+        String fraseInvertida = new StringBuilder(frase).reverse().toString();
+        
+        // Se compara la cadena frase con la cadena fraseInvertida. 
+        // Si son iguales, se muestra un mensaje indicando que la frase es un palíndromo. 
+        // Si no son iguales, se muestra un mensaje indicando que la frase no es un palíndromo.
+        if (frase.equals(fraseInvertida)) {
+            System.out.println("La frase es un palíndromo");
+        } else {
+            System.out.println("La frase no es un palíndromo");
+        }
+    }
+
+}
