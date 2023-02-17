ddd687b98443db750a2e7612dc344cb9100b777d
Mario Morales <92040099+mariomo16@users.noreply.github.com>
2023-02-17 12:43:42 +0000

    Actualizado Ejercicio2

--- UT03/Boletín II - Arrays/Ejercicio2.java
+++ UT03/Boletín II - Arrays/Ejercicio2.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
+
 public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // Pedimos los dos números por teclado
+        System.out.print("Introduzca el valor de N: ");
+        int n = sc.nextInt();
+        System.out.print("Introduzca el valor de M: ");
+        int m = sc.nextInt();
+
+        int[] numeros = new int[n];
+        int cantidad = 0;
+
+        // Pedimos los números por teclado y solo guardamos aquellos que sean múltiplos de M
+        for (int i = 0; i < n; i++) {
+            System.out.print("Introduzca el número " + (i+1) + ": ");
+            int num = sc.nextInt();
+            if (num % m == 0) {
+                numeros[cantidad] = num;
+                cantidad++;
+            }
+        }
+
+        // Mostramos el contenido del array
+        System.out.print("Array de múltiplos de " + m + ": ");
+        for (int i = 0; i < cantidad; i++) {
+            System.out.print(numeros[i] + " ");
+        }
+    }
     
 }
