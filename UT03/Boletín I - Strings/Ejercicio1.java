a7c960951f7c96b74f93ec684346d8a8398df378
Mario Morales <92040099+mariomo16@users.noreply.github.com>
2023-02-17 12:05:31 +0000

    asd

--- UT03/Boletín I - Strings/Ejercicio1.java
+++ UT03/Boletín I - Strings/Ejercicio1.java
@@ -1,15 +1,23 @@
 public class Ejercicio1 {
+
     public static void main(String[] args) {
+        // Declaración de 5 variables de tipo char
+        char var1 = 'H';
+        char var2 = 'o';
+        char var3 = 'l';
+        char var4 = 'a';
+        char var5 = '!';
 
-    char var1 = 'a';
-    char var2 = 'b';
-    char var3 = 'c';
-    char var4 = 'd';
-    char var5 = 'e';
+        // Concatenación de las variables char en una cadena
+        String cadena = "" + var1 + var2 + var3 + var4 + var5;
 
-    String cadena = "" + var1 + var2 + var3 + var4 + var5;
+        // Muestra la cadena resultante por pantalla
+        System.out.println(cadena);
 
-    System.out.println(cadena);
-    
+        // Si no se utiliza la cadena vacía "" antes de la primera variable, 
+        // el programa intentará sumar las variables de tipo char como enteros y no concatenarlas como cadenas, 
+        // lo que generará un error. Al utilizar la cadena vacía como primer operando, 
+        // se fuerza a Java a realizar una concatenación de cadenas en vez de una suma de enteros.
     }
+
 }
--- UT03/Boletín I - Strings/Ejercicio2.java
+++ UT03/Boletín I - Strings/Ejercicio2.java
@@ -1,11 +1,8 @@
-package boletinStrings;
-
 import java.util.Scanner;
 
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// Boletin strings
 	    Scanner sc = new Scanner(System.in);
 	    System.out.print("Introduce una cadena de texto: ");
 	    String cadena = sc.nextLine();
@@ -15,7 +12,6 @@ public class Ejercicio2 {
 	    for (String palabra : palabras) {
 	      System.out.println(palabra);
 	    }
-
 	}
 
 }
