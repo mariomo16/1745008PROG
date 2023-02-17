3e08f5205726d33c22a5e4913eaaad20f5375a73
Mario Morales <92040099+mariomo16@users.noreply.github.com>
2023-02-17 12:51:06 +0000

    Actualizado Ejercicio5

--- UT03/Boletín II - Arrays/Ejercicio5.java
+++ UT03/Boletín II - Arrays/Ejercicio5.java
@@ -1,3 +1,29 @@
 public class Ejercicio5 {
+
+    public static void main(String[] args) {
+        int[] array1 = new int[100];
+        int[] array2 = new int[100];
+
+        // Rellenamos el primer array con los valores del 1 al 100
+        for (int i = 0; i < 100; i++) {
+            array1[i] = i + 1;
+        }
+
+        // Copiamos los valores del primer array al segundo array en orden inverso
+        for (int i = 0; i < 100; i++) {
+            array2[i] = array1[99 - i];
+        }
+
+        // Mostramos los dos arrays por pantalla
+        System.out.println("Array 1:");
+        for (int i = 0; i < 100; i++) {
+            System.out.print(array1[i] + " ");
+        }
+
+        System.out.println("\n\nArray 2:");
+        for (int i = 0; i < 100; i++) {
+            System.out.print(array2[i] + " ");
+        }
+    }
     
 }
