ecacd9a5e0f94d212b6534b9b81270bb283ec2f3
Mario Morales <92040099+mariomo16@users.noreply.github.com>
2023-03-28 22:05:59 +0000

    Actualizado Agenda

--- UT06/Agenda/src/Contacto.java
+++ UT06/Agenda/src/Contacto.java
@@ -2,28 +2,35 @@ import java.time.LocalDate;
 
 public class Contacto {
 
-    public Contacto(String nombre, String apellidos, String telefono, String email, LocalDate fechaNacimiento) {
+    private String nombre, apellidos, telefono, email;
+    private LocalDate fechaNacimiento;
 
+    public Contacto(String nombre, String apellidos, String telefono, String email, LocalDate fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.email = email;
+        this.fechaNacimiento = fechaNacimiento;
     }
 
     public String getNombre() {
-        return null;
+        return nombre;
     }
 
     public String getApellidos() {
-        return null;
+        return apellidos;
     }
 
     public LocalDate getFechaNacimiento() {
-        return null;
+        return fechaNacimiento;
     }
 
     public String getTelefono() {
-        return null;
+        return telefono;
     }
 
     public String getEmail() {
-        return null;
+        return email;
     }
 
 }
