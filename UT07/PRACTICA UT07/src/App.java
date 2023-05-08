a93e74b09992ccac9f2c4a2b83160987939ae5d2
Mario Morales <mariomo16.cieza@gmail.com>
2023-05-08 09:30:55 +0000

    update App

--- UT07/PRACTICA UT07/src/App.java
+++ UT07/PRACTICA UT07/src/App.java
@@ -9,6 +9,7 @@ class App {
     static Scanner sc = new Scanner(System.in);
 
     static Publicaciones biblioteca = new Publicaciones();
+    
     public static void main(String[] args) throws Exception {
         
         System.out.println("Iniciando aplicación...");
