e342e3c1f5081eec0a94cf08738819dfb3d286a6
Mario Morales <mariomo16.cieza@gmail.com>
2023-05-30 21:06:13 +0000

    Correccion PRACTICA UT07

--- UT07/PRACTICA UT07/src/App.java
+++ UT07/PRACTICA UT07/src/App.java
@@ -8,10 +8,13 @@
  * 
  *  -- Al introducir valores incorrectos (o cualquier cosa que provoque un error, 
  *     se muestra un mensaje en pantalla "Ha ocurrido un error: ", seguido de el mensaje de error para poder identificarlo").
+ * 
  *     -- Ejemplo al introducir valores incorrectos en el formato de la fecha de publicacion:
  *        "Ha ocurrido un error: Data truncation: Incorrect date value: '202-1230' for column 'publish_date' at row 1"
+ * 
  *     -- En el metodo modificar(), por ejemplo, prefiero poner que al dar error, salga un mensaje asi, indicando que ha habido un error,
  *        seguido de una linea indicando que error ha dado, por si acaso el error que salta no es el del formato de fecha incorrecto.
+ *        No se si lo querias asi, o preferias que salga directamente algo parecido a "FORMATO DE FECHA INCORRECTO" (en este caso concreto).
  * 
  *  -- En principio, el unico error que me ha llegado a salir ha sido al introducir valores incorrectos en el formato de la fecha de publicacion,
  *     pero la app sigue funcionando sin problema, y solamente se muestra una unica linea indicando el error (para poder identificarlo).
